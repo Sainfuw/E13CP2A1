995861ce965c094070acf85f6b6c87163a9e5b3c
Sainfuw <saintlol@gmail.com>
2018-07-23 10:30:36 -0400

    ADD: completed and list

--- todo_app/app/controllers/todos_controller.rb
+++ todo_app/app/controllers/todos_controller.rb
@@ -34,6 +34,26 @@ class TodosController < ApplicationController
     redirect_to todos_path
   end
 
+  def complete
+    @todo = Todo.find(params[:id])
+    @todo.completed = true
+    @todo.save
+    redirect_to todos_path
+  end
+
+  def list
+    @todos = Todo.all
+    @completos = []
+    @incompletos = []
+    @todos.each do |todo|
+      if todo.completed
+        @completos << todo.description
+      else
+        @incompletos << todo.description
+      end
+    end
+  end
+
   private
   def todo_params
     params.require(:todo).permit(:description, :completed)

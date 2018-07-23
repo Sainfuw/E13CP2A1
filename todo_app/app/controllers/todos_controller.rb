7f1c2a32f7402536cb7bd399e0a269ec80e20740
Sainfuw <saintlol@gmail.com>
2018-07-23 09:12:53 -0400

    ADD: edit and update

--- todo_app/app/controllers/todos_controller.rb
+++ todo_app/app/controllers/todos_controller.rb
@@ -18,6 +18,16 @@ class TodosController < ApplicationController
     @completed = @todo.completed ? "Si" : "No"
   end
 
+  def edit
+    @todo = Todo.find(params[:id])
+  end
+
+  def update
+    @todo = Todo.find(params[:id])
+    @todo.update(todo_params)
+    redirect_to todos_path
+  end
+
   private
   def todo_params
     params.require(:todo).permit(:description, :completed)

96380bed681228ebb5807ce11383998b7afc3cf8
Sainfuw <saintlol@gmail.com>
2018-07-25 08:57:09 -0400

    Using resources

--- todo_app/config/routes.rb
+++ todo_app/config/routes.rb
@@ -1,12 +1,6 @@
 Rails.application.routes.draw do
   get 'todos/list'
-  get 'todos', to: 'todos#index'
-  get 'todos/new', to: 'todos#new', as: 'new_todo'
-  post 'todos', to: 'todos#create'
-  get 'todos/:id', to: 'todos#show', as: 'todo'
-  get 'todos/:id/edit', to: 'todos#edit', as: 'edit_todo'
-  patch 'todos/:id', to: 'todos#update'
-  delete 'todos/:id', to: 'todos#destroy'
+  resources :todos
   get 'todos/:id/complete', to: 'todos#complete', as: 'complete_todo'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
